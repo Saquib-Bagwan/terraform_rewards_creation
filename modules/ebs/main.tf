bf0f89df97dbeae76ba6935bb7908d3d3a8d9a50
Saquib Bagwan <saquibbagwan14@gmail.com>
2026-04-16 14:21:13 +0530

    issues solved from ebs

--- modules/ebs/main.tf
+++ modules/ebs/main.tf
@@ -1,25 +1,18 @@
-########################################
-# S3 Object (UPLOAD JAR)
-########################################
+# S3 Object 
 resource "aws_s3_object" "app" {
   bucket = var.s3_bucket
   key    = var.s3_key
 
-  # ✅ SAFE PATH
   source = "${path.root}/${var.app_path}"
   etag   = filemd5("${path.root}/${var.app_path}")
 }
 
-########################################
 # Elastic Beanstalk Application
-########################################
 resource "aws_elastic_beanstalk_application" "this" {
   name = var.app_name
 }
 
-########################################
 # Application Version
-########################################
 resource "aws_elastic_beanstalk_application_version" "this" {
   name        = var.app_version
   application = aws_elastic_beanstalk_application.this.name
@@ -27,12 +20,10 @@ resource "aws_elastic_beanstalk_application_version" "this" {
   bucket = var.s3_bucket
   key    = var.s3_key
 
-  depends_on = [aws_s3_object.app]   # ✅ IMPORTANT
+  depends_on = [aws_s3_object.app] 
 }
 
-########################################
 # IAM Role (Beanstalk Service Role)
-########################################
 resource "aws_iam_role" "ebs_service_role" {
   name = "${var.app_name}-service-role"
 
@@ -53,9 +44,7 @@ resource "aws_iam_role_policy_attachment" "ebs_service_policy" {
   policy_arn = "arn:aws:iam::aws:policy/service-role/AWSElasticBeanstalkService"
 }
 
-########################################
 # IAM Role (EC2 Instances)
-########################################
 resource "aws_iam_role" "ec2_role" {
   name = "${var.app_name}-ec2-role"
 
@@ -86,65 +75,50 @@ resource "aws_iam_role_policy_attachment" "multicontainer" {
   policy_arn = "arn:aws:iam::aws:policy/AWSElasticBeanstalkMulticontainerDocker"
 }
 
-########################################
 # Instance Profile (MANDATORY)
-########################################
 resource "aws_iam_instance_profile" "ec2_profile" {
   name = "${var.app_name}-ec2-profile"
   role = aws_iam_role.ec2_role.name
 }
 
-########################################
 # Elastic Beanstalk Environment
-########################################
 resource "aws_elastic_beanstalk_environment" "this" {
   name        = var.env_name
   application = aws_elastic_beanstalk_application.this.name
 
-  # ✅ UPDATED (no solution_stack_name)
   platform_arn = "arn:aws:elasticbeanstalk:ap-southeast-1::platform/Corretto 17 running on 64bit Amazon Linux 2/3.5.6"
 
   version_label = aws_elastic_beanstalk_application_version.this.name
 
-  ########################################
   # Instance Config
-  ########################################
   setting {
     namespace = "aws:autoscaling:launchconfiguration"
     name      = "InstanceType"
     value     = var.instance_type
   }
 
-  ########################################
   # Security Group
-  ########################################
   setting {
     namespace = "aws:ec2:vpc"
     name      = "SecurityGroups"
     value     = var.security_group_id
   }
 
-  ########################################
   # Environment Type
-  ########################################
   setting {
     namespace = "aws:elasticbeanstalk:environment"
     name      = "EnvironmentType"
     value     = "LoadBalanced"
   }
 
-  ########################################
   # Health Check
-  ########################################
   setting {
     namespace = "aws:elasticbeanstalk:application"
     name      = "Application Healthcheck URL"
     value     = "/"
   }
 
-  ########################################
   # Subnets (Optional)
-  ########################################
   dynamic "setting" {
     for_each = length(var.vpc_subnet_ids) > 0 ? [1] : []
     content {
@@ -163,9 +137,7 @@ resource "aws_elastic_beanstalk_environment" "this" {
     }
   }
 
-  ########################################
   # IAM Roles (CRITICAL)
-  ########################################
   setting {
     namespace = "aws:elasticbeanstalk:environment"
     name      = "ServiceRole"
@@ -178,9 +150,7 @@ resource "aws_elastic_beanstalk_environment" "this" {
     value     = aws_iam_instance_profile.ec2_profile.name
   }
 
-  ########################################
   # Dependencies (VERY IMPORTANT)
-  ########################################
   depends_on = [
     aws_iam_role_policy_attachment.ebs_service_policy,
     aws_iam_instance_profile.ec2_profile,
